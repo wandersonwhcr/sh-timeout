8b9b20c411655f1d5574ed2c54f039f27b3c34d4
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-01-18 07:56:39 -0300

    Initialize command

--- timeout.sh
+++ timeout.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env sh
+
+cleanup() {
+    kill -TERM "$PIDCOMMAND" "$PIDTIMEOUT"
+}
+
+trap cleanup TERM
+
+(sleep 5; kill -TERM $$) &
+PIDCOMMAND=$!
+(sleep 10; echo "Timeout"; kill -TERM $$) &
+PIDTIMEOUT=$!
+
+wait
