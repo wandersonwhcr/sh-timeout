1013d02b733815d62580f138398cbc9f34efb3f6
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-01-20 11:55:34 -0300

    Improve return code

--- timeout.sh
+++ timeout.sh
@@ -7,17 +7,30 @@ timeout() {
     (sleep "$TIMEOUT_SLEEP" && echo "Timeout.") &
     TIMEOUT_PID_TIMEOUT=$!
 
-    ("$@"; timeout_cleanup "$TIMEOUT_PID_TIMEOUT") &
+    timeout_execute "$@" &
     TIMEOUT_PID_COMMAND=$!
 
     wait "$TIMEOUT_PID_TIMEOUT"
     TIMEOUT_RC_TIMEOUT="$?"
 
     timeout_cleanup "$TIMEOUT_PID_COMMAND"
+
+    wait "$TIMEOUT_PID_COMMAND"
+    TIMEOUT_RC_COMMAND="$?"
+
+    echo "TIMEOUT_RC_TIMEOUT $TIMEOUT_RC_TIMEOUT"
+    echo "TIMEOUT_RC_COMMAND $TIMEOUT_RC_COMMAND"
 }
 
 timeout_cleanup() {
     ps -o pid= --ppid "$1" | xargs -r kill -TERM
+}
+
+timeout_execute() {
+    "$@"
+    TIMEOUT_RC_COMMAND="$?"
+    timeout_cleanup "$TIMEOUT_PID_TIMEOUT"
+    return "$TIMEOUT_RC_COMMAND"
 }
 
 timeout "$@"
